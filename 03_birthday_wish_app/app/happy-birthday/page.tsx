48f556a40a70bea01858601bcf850b8011358763
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2024-09-04 17:29:11 +0500

    Create birthday-wish-app

--- 03_birthday_wish_app/app/happy-birthday/page.tsx
+++ 03_birthday_wish_app/app/happy-birthday/page.tsx
@@ -1,7 +1,7 @@
 import BirthdayWish from '@/components/BirthdayWish'
 import React from 'react'
 
-export default function page(params: {name: string, dob: string, age: number}) {
+export default function page() {
   return (
     <BirthdayWish />
   )
