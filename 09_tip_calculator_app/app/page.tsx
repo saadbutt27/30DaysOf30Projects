07c53905f3dc794044499c9850883fd7159287c5
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2024-09-10 10:28:35 +0500

    Created tip calculator app

---
+++ 09_tip_calculator_app/app/page.tsx
@@ -0,0 +1,10 @@
+import TipCalculator from "@/components/TipCalculator";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <TipCalculator />
+    </main>
+  );
+}
